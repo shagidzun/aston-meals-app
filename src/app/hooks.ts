ce0f561015f0806392f949fc74c4136835d99414
Erdni Zungruev <shagidzun@gmail.com>
2024-02-29 16:32:21 +0300

    feat: add useDebounce hook to hooks.ts

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -5,8 +5,24 @@
 // for importing and re-exporting the typed versions of hooks.
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import type { AppDispatch, RootState } from "./store";
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+export const useDebounce = <T>(value: T, delay: number): T => {
+	const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+	useEffect(() => {
+		const timoutId = setTimeout(() => {
+			setDebouncedValue(value);
+		}, delay);
+
+		return () => {
+			clearTimeout(timoutId);
+		};
+	}, [value, delay]);
+
+	return debouncedValue;
+};
