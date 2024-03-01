8a51380de7cd64c73de42e3b4c6bedee4325c473
Erdni Zungruev <shagidzun@gmail.com>
2024-03-01 17:14:10 +0300

    feat: add SignIn and SignUp pages

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const Navigation = () => {
@@ -11,7 +11,27 @@ export const Navigation = () => {
 				padding: "20px"
 			}}
 		>
-			<Link to={"/"}>Home</Link>
+			<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+				<Link style={{ textDecoration: "none", color: "inherit" }} to={"/"}>
+					<Button sx={{ my: 2, color: "white", display: "block" }}>Home</Button>
+				</Link>
+				<Link
+					style={{ textDecoration: "none", color: "inherit" }}
+					to={"/signup"}
+				>
+					<Button sx={{ my: 2, color: "white", display: "block" }}>
+						Sign up
+					</Button>
+				</Link>
+				<Link
+					style={{ textDecoration: "none", color: "inherit" }}
+					to={"/signin"}
+				>
+					<Button sx={{ my: 2, color: "white", display: "block" }}>
+						Sign in
+					</Button>
+				</Link>
+			</Box>
 		</AppBar>
 	);
 };
---
+++ src/pages/signin/SignIn.tsx
@@ -0,0 +1,60 @@
+import Container from "@mui/material/Container";
+import {
+	FormControl,
+	IconButton,
+	InputAdornment,
+	InputLabel,
+	OutlinedInput,
+	Stack,
+	Typography
+} from "@mui/material";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { Navigation } from "../../components/navigation/Navigation";
+
+export const SignIn = () => {
+	const [showPassword, setShowPassword] = useState<boolean>(false);
+	const handleClickShowPassword = () => setShowPassword(show => !show);
+	const handleMouseDownPassword = (
+		event: React.MouseEvent<HTMLButtonElement>
+	) => {
+		event.preventDefault();
+	};
+	return (
+		<>
+			<Navigation />
+			<Container maxWidth="sm">
+				<Stack>
+					<FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+						<InputLabel htmlFor="login">Login</InputLabel>
+						<OutlinedInput id="login" label="Login" />
+					</FormControl>
+					<FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+						<InputLabel htmlFor="password">Password</InputLabel>
+						<OutlinedInput
+							id="password"
+							type={showPassword ? "text" : "password"}
+							endAdornment={
+								<InputAdornment position="end">
+									<IconButton
+										aria-label="toggle password visibility"
+										onClick={handleClickShowPassword}
+										onMouseDown={handleMouseDownPassword}
+										edge="end"
+									>
+										{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+							}
+							label="Password"
+						/>
+					</FormControl>
+					<Typography>
+						Don't have an account? <Link to="/signup">Sign up</Link>
+					</Typography>
+				</Stack>
+			</Container>
+		</>
+	);
+};
