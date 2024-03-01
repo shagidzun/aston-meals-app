6e62dfda238c3d602cb5e1b024947c9e6c089729
Erdni Zungruev <shagidzun@gmail.com>
2024-03-01 13:20:47 +0300

    feat: add loading indicator

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,46 +2,56 @@ import Container from "@mui/material/Container";
 import {
 	Avatar,
 	Divider,
+	LinearProgress,
 	List,
 	ListItem,
 	ListItemAvatar,
-	ListItemText
+	ListItemText,
+	Typography
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Fragment } from "react";
 import { Navigation } from "../../components/navigation/Navigation";
 import { useGetMealsCategoriesQuery } from "../../services/mealsApi";
+import { SearchField } from "../../components/search/SearchField";
 
 export const Home = () => {
-	const { data, isError, isSuccess } = useGetMealsCategoriesQuery();
+	const { data, isError, isLoading } = useGetMealsCategoriesQuery();
 	return (
 		<>
 			<Navigation />
+			<SearchField />
 			<Container maxWidth="sm">
-				<List
-					sx={{
-						width: "100%",
-						maxWidth: "sm",
-						padding: "0px"
-					}}
-				>
-					{data?.categories.map((category, i) => (
-						<Fragment key={category.idCategory}>
-							{i !== 0 && <Divider component="li" />}
-							<Link to={`/category/${category.strCategory}`}>
-								<ListItem>
-									<ListItemAvatar>
-										<Avatar
-											src={category.strCategoryThumb}
-											alt={category.strCategory}
-										/>
-									</ListItemAvatar>
-									<ListItemText primary={category.strCategory} />
-								</ListItem>
-							</Link>
-						</Fragment>
-					))}
-				</List>
+				{isLoading ? (
+					<LinearProgress />
+				) : isError ? (
+					<Typography variant="h5">Something went wrong :(</Typography>
+				) : (
+					<List
+						sx={{
+							width: "100%",
+							maxWidth: "sm",
+							padding: "0px"
+						}}
+					>
+						{data?.categories.map((category, i) => (
+							<Fragment key={category.idCategory}>
+								{i !== 0 && <Divider component="li" />}
+								<Link to={`/category/${category.strCategory}`}>
+									<ListItem>
+										<ListItemAvatar>
+											<Avatar
+												src={category.strCategoryThumb}
+												alt={category.strCategory}
+											/>
+										</ListItemAvatar>
+										<ListItemText primary={category.strCategory} />
+									</ListItem>
+								</Link>
+							</Fragment>
+						))}
+					</List>
+				)}
 			</Container>
 		</>
 	);
--- src/pages/meal/Meal.tsx
+++ src/pages/meal/Meal.tsx
@@ -1,6 +1,7 @@
 import Container from "@mui/material/Container";
 import {
 	Box,
+	LinearProgress,
 	Stack,
 	Table,
 	TableBody,
@@ -14,61 +15,69 @@ import { useParams } from "react-router-dom";
 import { useGetMealByIdQuery } from "../../services/mealsApi";
 import { Navigation } from "../../components/navigation/Navigation";
 import { filterProps } from "../../utils/filterProps";
+import { SearchField } from "../../components/search/SearchField";
 
 export const Meal = () => {
 	const { id } = useParams();
-	const { data, isError, isSuccess } = useGetMealByIdQuery(id);
+	const { data, isError, isLoading } = useGetMealByIdQuery(id);
 	const meal = data?.meals[0];
 	const ingredients = filterProps(meal, "strIngredient");
 	const measures = filterProps(meal, "strMeasure");
 	return (
 		<>
 			<Navigation />
+			<SearchField />
 			<Container>
-				<Stack spacing={2}>
-					<Box
-						sx={{
-							display: "flex",
-							justifyContent: "space-between",
-							alignItems: "top"
-						}}
-					>
-						<figure>
-							<img
-								src={meal?.strMealThumb}
-								alt={meal?.strMeal}
-								style={{ width: 300 }}
-							/>
-							<figcaption>
-								<Typography>{meal?.strMeal}</Typography>
-							</figcaption>
-						</figure>
-						<TableContainer>
-							<Table size="small">
-								<TableHead>
-									<TableRow>
-										<TableCell>Ingredients</TableCell>
-										<TableCell>Measure</TableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{ingredients.map((ingredient, i) => (
-										<TableRow key={i}>
-											<TableCell>{ingredient}</TableCell>
-											<TableCell>{measures[i]}</TableCell>
+				{isLoading ? (
+					<LinearProgress />
+				) : isError ? (
+					<Typography variant="h5">Something went wrong :(</Typography>
+				) : (
+					<Stack spacing={2}>
+						<Box
+							sx={{
+								display: "flex",
+								justifyContent: "space-between",
+								alignItems: "top"
+							}}
+						>
+							<figure>
+								<img
+									src={meal?.strMealThumb}
+									alt={meal?.strMeal}
+									style={{ width: 300 }}
+								/>
+								<figcaption>
+									<Typography>{meal?.strMeal}</Typography>
+								</figcaption>
+							</figure>
+							<TableContainer>
+								<Table size="small">
+									<TableHead>
+										<TableRow>
+											<TableCell>Ingredients</TableCell>
+											<TableCell>Measure</TableCell>
 										</TableRow>
-									))}
-								</TableBody>
-							</Table>
-						</TableContainer>
-					</Box>
-					<Box>
-						<Typography variant="h3" gutterBottom>
-							Instructions
-						</Typography>
-						<Typography variant="body1">{meal?.strInstructions}</Typography>
-					</Box>
-				</Stack>
+									</TableHead>
+									<TableBody>
+										{ingredients.map((ingredient, i) => (
+											<TableRow key={i}>
+												<TableCell>{ingredient}</TableCell>
+												<TableCell>{measures[i]}</TableCell>
+											</TableRow>
+										))}
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</Box>
+						<Box>
+							<Typography variant="h3" gutterBottom>
+								Instructions
+							</Typography>
+							<Typography variant="body1">{meal?.strInstructions}</Typography>
+						</Box>
+					</Stack>
+				)}
 			</Container>
 		</>
 	);
