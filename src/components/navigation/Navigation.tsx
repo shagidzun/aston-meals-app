a8788532757ae8e40e54ffbf29b92925072d11e7
Erdni Zungruev <shagidzun@gmail.com>
2024-03-02 17:42:04 +0300

    feat: add user email and sign out button, persist current user

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,36 +1,95 @@
-import { AppBar, Box, Button } from "@mui/material";
+import { AppBar, Box, Button, Typography } from "@mui/material";
+import { getAuth, signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
-
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+	removeUser,
+	selectIsAuth,
+	setUser
+} from "../../features/user/userSlice";
+// временно нагрузил навигацию, на стадии рефакторинга исправлю
 export const Navigation = () => {
+	const isAuth = useAppSelector(selectIsAuth);
+	const auth = getAuth();
+	const dispatch = useAppDispatch();
+	const handleOnSignOut = () => {
+		signOut(auth)
+			.then(() => {
+				dispatch(removeUser());
+				console.log(auth.currentUser);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+	useEffect(() => {
+		const auth = getAuth();
+		auth.onAuthStateChanged(user => {
+			if (user) {
+				user.getIdToken().then(token => {
+					dispatch(setUser(user.email, token, user.uid));
+				});
+			}
+		});
+	}, [dispatch]);
 	return (
 		<AppBar
 			sx={{
 				height: 50,
-				justifyContent: "center",
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-between",
 				position: "static",
 				padding: "20px"
 			}}
 		>
-			<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+			<Box
+				sx={{
+					flexGrow: 1,
+					display: { xs: "none", md: "flex", alignItems: "center" }
+				}}
+			>
 				<Link style={{ textDecoration: "none", color: "inherit" }} to={"/"}>
 					<Button sx={{ my: 2, color: "white", display: "block" }}>Home</Button>
 				</Link>
-				<Link
-					style={{ textDecoration: "none", color: "inherit" }}
-					to={"/signup"}
-				>
-					<Button sx={{ my: 2, color: "white", display: "block" }}>
-						Sign up
-					</Button>
-				</Link>
-				<Link
-					style={{ textDecoration: "none", color: "inherit" }}
-					to={"/signin"}
-				>
-					<Button sx={{ my: 2, color: "white", display: "block" }}>
-						Sign in
-					</Button>
-				</Link>
+			</Box>
+			<Box
+				sx={{
+					flexGrow: 0,
+					display: { xs: "none", md: "flex", alignItems: "center" }
+				}}
+			>
+				{isAuth ? (
+					<>
+						<Typography>{auth.currentUser?.email}</Typography>
+						<Button
+							sx={{ my: 2, color: "white", display: "block" }}
+							onClick={handleOnSignOut}
+						>
+							Sign out
+						</Button>
+					</>
+				) : (
+					<>
+						<Link
+							style={{ textDecoration: "none", color: "inherit" }}
+							to={"/signup"}
+						>
+							<Button sx={{ my: 2, color: "white", display: "block" }}>
+								Sign up
+							</Button>
+						</Link>
+						<Link
+							style={{ textDecoration: "none", color: "inherit" }}
+							to={"/signin"}
+						>
+							<Button sx={{ my: 2, color: "white", display: "block" }}>
+								Sign in
+							</Button>
+						</Link>
+					</>
+				)}
 			</Box>
 		</AppBar>
 	);
