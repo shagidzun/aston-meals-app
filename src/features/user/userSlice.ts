9b85670de04e9f16ebbe732cb08f157dc83a56c8
Erdni Zungruev <shagidzun@gmail.com>
2024-03-02 17:36:42 +0300

    fix: add isAuth prop, fix reducers, add selector

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,14 +1,15 @@
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { createSelector } from "@reduxjs/toolkit";
 import { createAppSlice } from "../../app/createAppSlice";
 
 export interface UserSliceState {
+	isAuth: boolean;
 	email: string | null;
 	token: string | null;
 	id: string | null;
 }
 
 const initialState: UserSliceState = {
+	isAuth: false,
 	email: null,
 	token: null,
 	id: null
@@ -17,17 +18,30 @@ export const userSlice = createAppSlice({
 	name: "user",
 	initialState,
 	reducers: create => ({
-		setUser: (state, action: PayloadAction<UserSliceState>) => {
-			state.email = action.payload.email;
-			state.token = action.payload.token;
-			state.id = action.payload.id;
-		},
-		removeUser: state => {
+		setUser: create.preparedReducer(
+			(email: string | null, token: string, id: string) => {
+				return {
+					payload: { isAuth: true, email, token, id }
+				};
+			},
+			(state, action: PayloadAction<UserSliceState>) => {
+				state.isAuth = action.payload.isAuth;
+				state.email = action.payload.email;
+				state.token = action.payload.token;
+				state.id = action.payload.id;
+			}
+		),
+		removeUser: create.reducer(state => {
+			state.isAuth = false;
 			state.email = null;
 			state.token = null;
 			state.id = null;
-		}
-	})
+		})
+	}),
+	selectors: {
+		selectIsAuth: user => user.isAuth
+	}
 });
 
 export const { setUser, removeUser } = userSlice.actions;
+export const { selectIsAuth } = userSlice.selectors;
