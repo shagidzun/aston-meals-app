f3eaeec4e6a8ec913449850188391b6a196aeeb0
Erdni Zungruev <shagidzun@gmail.com>
2024-03-02 17:38:14 +0300

    feat: add firebase sign in/sign up

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,59 +1,31 @@
 import Container from "@mui/material/Container";
-import {
-	FormControl,
-	IconButton,
-	InputAdornment,
-	InputLabel,
-	OutlinedInput,
-	Stack,
-	Typography
-} from "@mui/material";
-import { useState } from "react";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { Navigation } from "../../components/navigation/Navigation";
+import { Form } from "../../components/form/Form";
+import { useAppDispatch } from "../../app/hooks";
+import { setUser } from "../../features/user/userSlice";
 
 export const SignUp = () => {
-	const [showPassword, setShowPassword] = useState<boolean>(false);
-	const handleClickShowPassword = () => setShowPassword(show => !show);
-	const handleMouseDownPassword = (
-		event: React.MouseEvent<HTMLButtonElement>
-	) => {
-		event.preventDefault();
+	const dispatch = useAppDispatch();
+	const auth = getAuth();
+	const navigate = useNavigate();
+	const handleSignUp = (email: string, password: string) => {
+		createUserWithEmailAndPassword(auth, email, password)
+			.then(userCredential => {
+				const user = userCredential.user;
+				user.getIdToken().then(token => {
+					dispatch(setUser(user.email, token, user.uid));
+				});
+				navigate("/");
+			})
+			.catch(console.log); //временно, позже будет показ ошибки в ui
 	};
 	return (
 		<>
 			<Navigation />
 			<Container maxWidth="sm">
-				<Stack>
-					<FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-						<InputLabel htmlFor="login">Login</InputLabel>
-						<OutlinedInput id="login" label="Login" />
-					</FormControl>
-					<FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-						<InputLabel htmlFor="password">Password</InputLabel>
-						<OutlinedInput
-							id="password"
-							type={showPassword ? "text" : "password"}
-							endAdornment={
-								<InputAdornment position="end">
-									<IconButton
-										aria-label="toggle password visibility"
-										onClick={handleClickShowPassword}
-										onMouseDown={handleMouseDownPassword}
-										edge="end"
-									>
-										{showPassword ? <VisibilityOff /> : <Visibility />}
-									</IconButton>
-								</InputAdornment>
-							}
-							label="Password"
-						/>
-					</FormControl>
-					<Typography>
-						Already have an account? <Link to="/signin">Sign in</Link>
-					</Typography>
-				</Stack>
+				<Form title={"Sign up"} handleSubmit={handleSignUp} />
 			</Container>
 		</>
 	);
