d473716175f2eaa98ce06f4407b22c5900e8459e
Erdni Zungruev <shagidzun@gmail.com>
2024-03-03 17:08:17 +0300

    feat: add signed up user to firestore db

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,10 +1,12 @@
 import Container from "@mui/material/Container";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 import { Navigation } from "../../components/navigation/Navigation";
 import { Form } from "../../components/form/Form";
 import { useAppDispatch } from "../../app/hooks";
 import { setUser } from "../../features/user/userSlice";
+import { db } from "../../firebase/firebase";
 
 export const SignUp = () => {
 	const dispatch = useAppDispatch();
@@ -16,6 +18,11 @@ export const SignUp = () => {
 				const user = userCredential.user;
 				user.getIdToken().then(token => {
 					dispatch(setUser(user.email, token, user.uid));
+				});
+				setDoc(doc(db, "users", user.uid), {
+					email: user.email,
+					id: user.uid,
+					date: new Date().toISOString()
 				});
 				navigate("/");
 			})
