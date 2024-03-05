e428bed179cdf23f98edc83442cee5bbac84de2a
Erdni Zungruev <shagidzun@gmail.com>
2024-03-05 18:10:35 +0300

    fix: fix middleware

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,12 +1,20 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
+import { signOut } from "firebase/auth";
+import { userSignOut } from "../features/user/userSlice";
+import { clearHistory } from "../features/history/historySlice";
 import { clearFavorites } from "../features/favorites/favoritesSlice";
-import { removeUser } from "../features/user/userSlice";
+import { auth } from "../firebase/firebase";
 
 export const listenerMiddleware = createListenerMiddleware();
-
+/*На мой взгляд было бы лучше сделать это через create.asyncThunk,
+как было сделано в редьюсере userSignIn в userSlice.
+Но сделал кастомную мидлвару, чтобы закрыть соответствующее требование*/
 listenerMiddleware.startListening({
-	actionCreator: removeUser,
-	effect: (action, api) => {
-		console.log("actionCreator", action);
+	actionCreator: userSignOut,
+	effect: async (action, api) => {
+		const dispatch = api.dispatch;
+		await signOut(auth);
+		dispatch(clearHistory());
+		dispatch(clearFavorites());
 	}
 });
