877567987e5d22ad2ce2c1e1b0acf6b613ab2b6d
Erdni Zungruev <shagidzun@gmail.com>
2024-03-05 18:28:39 +0300

    refactor: remove comment

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -6,9 +6,7 @@ import { clearFavorites } from "../features/favorites/favoritesSlice";
 import { auth } from "../firebase/firebase";
 
 export const listenerMiddleware = createListenerMiddleware();
-/*На мой взгляд было бы лучше сделать это через create.asyncThunk,
-как было сделано в редьюсере userSignIn в userSlice.
-Но сделал кастомную мидлвару, чтобы закрыть соответствующее требование*/
+
 listenerMiddleware.startListening({
 	actionCreator: userSignOut,
 	effect: async (action, api) => {
